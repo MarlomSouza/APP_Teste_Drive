9af5b1a27b32cf7421a0094e3663c5869d0f621b
Marlom Souza <thunder_bolt2000@hotmail.com>
2017-10-07 22:50:50 -0400

    Pacotes

---
+++ TesteDrive/TesteDrive/Business/BaseBusiness.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TesteDrive.Business
+{
+    public abstract class BaseBusiness<T>
+    {
+        public string URL { get; set; }
+        protected HttpClient httpcliente = new HttpClient();
+        public BaseBusiness(string URL)
+        {
+            this.URL = URL;
+        }
+
+        public abstract void Salvar(T t);
+        public abstract void Obter();
+        public abstract void Obter(string id);
+        public abstract void Excluir(string id);
+
+
+    }
+}
